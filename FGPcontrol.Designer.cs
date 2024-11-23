8206a943a8a8c18b326c67002d749ab1ce543701
lam96 <lam96@ECOTEKSUPPORTER>
2024-11-23 17:13:07 +0700

    Add project files.

---
+++ FGPcontrol.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace SmartCard
+{
+    partial class FGPcontrol
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox_showFGPicon = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox_showFGPicon).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox_showFGPicon
+            // 
+            pictureBox_showFGPicon.Location = new Point(3, 3);
+            pictureBox_showFGPicon.Name = "pictureBox_showFGPicon";
+            pictureBox_showFGPicon.Size = new Size(93, 84);
+            pictureBox_showFGPicon.TabIndex = 0;
+            pictureBox_showFGPicon.TabStop = false;
+            // 
+            // FGPcontrol
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(pictureBox_showFGPicon);
+            Name = "FGPcontrol";
+            Size = new Size(479, 251);
+            ((System.ComponentModel.ISupportInitialize)pictureBox_showFGPicon).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox_showFGPicon;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SmartCard
+{
+    public partial class Form1 : Form
+    {
+        FingerprintSensor _fingerprintSensor = new FingerprintSensor();
+        
+        public Form1()
+        {
+            InitializeComponent();
+            _fingerprintSensor.VerifyPassword();
+            _fingerprintSensor.SetSecurityLevel(4);
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var haveFinger = _fingerprintSensor.ReadImage();
+            if (haveFinger)
+            {
+                _fingerprintSensor.ConvertImage(1);
+                _fingerprintSensor.ReadImage();
+                _fingerprintSensor.ConvertImage(2);
+                _fingerprintSensor.CreateTemplate();
+
+                _fingerprintSensor.ReadImage();
+                _fingerprintSensor.ConvertImage(2);
+                _fingerprintSensor.CreateTemplate();
+
+                var characteristic = _fingerprintSensor.DownloadCharacteristics(1);
+            }
+
+        }
+    }
+}
